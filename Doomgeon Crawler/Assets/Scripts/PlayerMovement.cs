d00f3aacf6ce258f7a48d2a304c5ead84cfdf3c0
Christina <catchristina99@gmail.com>
2026-01-27 11:08:37 +0000

    Update

---
+++ Doomgeon Crawler/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField] private float baseMoveSpeed;
+    [SerializeField] private float moveSpeedMult;
+
+    private float moveX;
+    private float moveY;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        moveX = 
+    }
+
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        rb.linearVelocity = new Vector3 (moveX * moveSpeedMult, moveY * moveSpeedMult);
+    }
+}
